01d9e2703c30f75195ecc5daa3070f818f4dd293
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 17:55:33 -0300

    Create Main.java

---
+++ Exercicio02/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Salario novoSalario= new Salario();
+        novoSalario.entrada();
+        novoSalario.classificar();
+        novoSalario.imprimir();
+    }
+   
+}
