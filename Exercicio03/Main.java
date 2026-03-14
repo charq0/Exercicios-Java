b57f1a6b4bc0c300ee72bb642895246e854ca8e5
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 17:58:26 -0300

    Create Main.java

---
+++ Exercicio03/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa novaPessoa= new Pessoa();
+        
+        novaPessoa.entrada();
+        novaPessoa.calculoPeso();
+        novaPessoa.impressao();
+    }
+    
+}
+
