aacddbb9f79b724eecf7ad9f8613a12228d3c409
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-14 17:31:35 -0300

    Criar Main1.java

---
+++ Main1.java
@@ -0,0 +1,10 @@
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        Salario novoSalario= new Salario();
+        novoSalario.entrada();
+        novoSalario.atualizar();
+        novoSalario.imprimir();
+    }
+   
+}
